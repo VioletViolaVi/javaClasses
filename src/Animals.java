54557a953caf4e77b063271577b421d3cb1fc99c
VioletViola <vivian.v18723@gmail.com>
2021-08-22 23:56:07 +0100

    Edit constructor in Animals.java file.

--- src/Animals.java
+++ src/Animals.java
@@ -1,10 +1,15 @@
 public class Animals {
-    String catSpeak;
-    int cowNums;
+    String catSpeak = "Meow! Meow!";
+    int cowNums = 47;
 
-    public Animals() {
-        this.catSpeak = "Meow! Meow!";
-        this.cowNums = 47;
+    String animalType;
+    int numOfAnimals;
+    boolean hasBeenFed;
+
+    public Animals(String animalType, int numOfAnimals, boolean hasBeenFed) {
+        this.animalType = animalType;
+        this.numOfAnimals = numOfAnimals;
+        this.hasBeenFed = hasBeenFed;
     }
 
     public void DogSpeak(){
