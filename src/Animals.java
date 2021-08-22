5574009a0698686b46e313cccdc7874ca6adec34
VioletViola <vivian.v18723@gmail.com>
2021-08-22 23:44:58 +0100

    Create cat instance variable and constructor.

--- src/Animals.java
+++ src/Animals.java
@@ -1,4 +1,10 @@
 public class Animals {
+    String catSpeak;
+
+    public Animals() {
+        this.catSpeak = "Meow! Meow!";
+    }
+
     public void DogSpeak(){
         System.out.println("Woof! Woof!");
     }
