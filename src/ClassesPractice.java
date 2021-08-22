c62f697cb8fbe6b73e7cbb278d6245cf3c4f4289
VioletViola <vivian.v18723@gmail.com>
2021-08-22 23:46:46 +0100

    Called cat variable in main method.

--- src/ClassesPractice.java
+++ src/ClassesPractice.java
@@ -2,5 +2,7 @@ public class ClassesPractice {
     public static void main(String[] args) {
         Animals animals = new Animals();
         animals.DogSpeak();
+        String catSays = animals.catSpeak;
+        System.out.println(catSays);
     }
 }
