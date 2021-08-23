e73ce672e25a5d7a6400704974e3c1ca0fe2696b
VioletViola <vivian.v18723@gmail.com>
2021-08-23 16:08:01 +0100

    Create method in Foods class.

--- src/Foods.java
+++ src/Foods.java
@@ -1,19 +1,29 @@
+import java.awt.*;
+
 public class Foods {
     String foodName;
-    boolean orderOrNot;
+    boolean chosen;
     int quantity;
     double singularPrice;
     double totalCost;
 
-    Foods(String foodName, boolean orderOrNot, int quantity, double singularPrice){
+    Foods(String foodName, boolean chosen, int quantity, double singularPrice){
         this.foodName = foodName;
-        this.orderOrNot = orderOrNot;
+        this.chosen = chosen;
         this.quantity = quantity;
         this.singularPrice = singularPrice;
         this.totalCost = quantity * singularPrice;
         System.out.println("Here is your complete order!");
     }
 
-
+    public String orderedOrNot() {
+        String order = "";
+        if (this.chosen) {
+            order = "You've chosen to buy " + this.quantity + " " + this.foodName + "'s.";
+        } else {
+            order = "Please place your order!";
+        }
+        return order;
+    }
 
 }
